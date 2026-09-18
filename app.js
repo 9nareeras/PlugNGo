b658fea94effc976d9e5fac86717b0afedc1f093
mori <nannapas.mori@gmail.com>
2026-09-18 23:23:43 +0700

    setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({ 
+        message: "PlugNgo API is running successfully!",
+        status: "OK"
+    });
+});
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
